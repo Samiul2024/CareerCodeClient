14a17c7f5d1fb2295367a41396ce71d8fdede24f
Samiul2024 <mdsamiullahossen@gmail.com>
2026-02-05 22:36:17 +0600

    created job apply form...

--- src/pages/JobApply/JobApply.jsx
+++ src/pages/JobApply/JobApply.jsx
@@ -7,9 +7,51 @@ const JobApply = () => {
     const { user } = useAuth()
     // console.log(params);
     console.log(jobId, user);
+
+    const handleApplyFormSubmit = e => {
+        e.preventDefault();
+        const form = e.target;
+        const linkedIn = form.linkedIn.value;
+        const github = form.github.value;
+        const resume = form.resume.value;
+
+        console.log(linkedIn, github, resume);
+
+    }
+
+
     return (
         <div>
             <h3 className="text-4xl">Apply Job For</h3>
+            <form onSubmit={handleApplyFormSubmit}>
+                <fieldset className='fieldset bg-base-200 border-base-300 rounded-box w-xs'>
+                    <label className='label'>
+                        LinkedIn Link
+                    </label>
+                    <input type="url"
+                        name="linkedIn" className="input" placeholder="LinkedIn profile Link">
+                    </input>
+
+                    <label className='label'>
+                        Github Link
+                    </label>
+                    <input type="url"
+                        name="github"
+                        className="input" placeholder="Github profile Link">
+                    </input>
+
+                    <label className='label'>
+                        Resume Link
+                    </label>
+                    <input type="url"
+                        name="resume"
+                        className="input" placeholder="Resume Link">
+                    </input>
+
+                    <input type="submit" className='btn' value="Apply" />
+
+                </fieldset>
+            </form>
         </div>
     );
 };
