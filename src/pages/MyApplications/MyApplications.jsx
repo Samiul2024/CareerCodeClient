2affd1529f166672a875d32db0af44393c476922
Samiul2024 <mdsamiullahossen@gmail.com>
2026-02-08 00:42:12 +0600

    applied jobs shown in the ui from backend server

--- src/pages/MyApplications/MyApplications.jsx
+++ src/pages/MyApplications/MyApplications.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ApplicationStats from './ApplicationStats';
 import ApplicationList from './ApplicationList';
+import useAuth from '../../hooks/useAuth';
+import { myApplicationsPromise } from '../../api/applicationsApi';
+
+
+// const myApplicationsPromise = email => {
+//     return fetch(`http://localhost:3000/applications?email=${email}`).then(res = res.json())
+// }
+// const myApplicationPromise=fetch(``)
 
 const MyApplications = () => {
+
+
+    const { user } = useAuth();
+
+
     return (
         <div>
             <ApplicationStats></ApplicationStats>
-            <ApplicationList></ApplicationList>
+            <Suspense fallback={'loading your applications'}>
+                <ApplicationList
+                    myApplicationsPromise={myApplicationsPromise(user.email)}
+                ></ApplicationList>
+            </Suspense>
         </div>
     );
 };
