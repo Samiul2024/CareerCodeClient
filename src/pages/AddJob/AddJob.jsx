131d6f18f3f3fe675fbc7f126c592ef28e1dc09a
Samiul2024 <mdsamiullahossen@gmail.com>
2026-02-10 23:39:45 +0600

    got formData...

--- src/pages/AddJob/AddJob.jsx
+++ src/pages/AddJob/AddJob.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
 const AddJob = () => {
+
+
+    const handleAddAJob = e => {
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form);
+        // console.log(formData.entries());
+
+        const data=Object.fromEntries(formData.entries());
+        console.log(data);
+    }
+
+
     return (
         <div>
             <h2>Please add a job</h2>
-            <form>
+            <form onSubmit={handleAddAJob}>
                 <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
                     <legend className="fieldset-legend">Basic Info</legend>
 
@@ -61,46 +74,75 @@ const AddJob = () => {
                 <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
                     <legend className="fieldset-legend">Salary range</legend>
 
-                    <label className="label">Minimum Salary</label>
-                    <input type="text"
-                        name='salaryMin'
-                        className="input" placeholder="Minimum Salary" />
 
-                    <label className="label">Maximum Salary</label>
-                    <input type="text"
-                        name='salaryMax'
-                        className="input" placeholder="Maximum Salary" />
+                    <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
+                        <div>
+                            <label className="label">Minimum Salary</label>
+                            <input type="text"
+                                name='min'
+                                className="input" placeholder="Minimum Salary" />
+                        </div>
 
-                    <label className="label">Currency</label>
+                        <div>
+                            <label className="label">Maximum Salary</label>
+                            <input type="text"
+                                name='max'
+                                className="input" placeholder="Maximum Salary" />
 
-                    <select defaultValue="Select a currency"
-                        name='category'
-                        className="select">
-                        <option disabled={true}>Select a currency</option>
-                        <option>BDT</option>
-                        <option>USD</option>
-                        <option>EUR</option>
-                        <option>YUN</option>
-                    </select>    
+                        </div>
+                        <div>
+                            <label className="label">Currency</label>
 
-                    <input type="text" className="input" placeholder="Name" />
+                            <select defaultValue="Select a currency"
+                                name='currency'
+                                className="select">
+                                <option disabled={true}>Select a currency</option>
+                                <option>BDT</option>
+                                <option>USD</option>
+                                <option>EUR</option>
+                                <option>YUN</option>
+                            </select>
+                        </div>
+                    </div>
+                    {/* 
+                    <input type="text" className="input" placeholder="Name" /> */}
                 </fieldset>
 
-                {/*  */}
+                {/*Job Description  */}
                 <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-                    <legend className="fieldset-legend">Basic Info</legend>
+                    <legend className="fieldset-legend">Job Description</legend>
+                    <textarea className="textarea" placeholder="Job Description"></textarea>
 
                 </fieldset>
-                {/*  */}
+                {/* Job Requirements */}
                 <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-                    <legend className="fieldset-legend">Basic Info</legend>
+                    <legend className="fieldset-legend">Job Requirements</legend>
+                    <textarea className="textarea"
+                        name='requirements' placeholder="Requirements (separate by comma)"></textarea>
 
                 </fieldset>
-                {/*  */}
+                {/* Job Responsibilities */}
                 <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-                    <legend className="fieldset-legend">Basic Info</legend>
+                    <legend className="fieldset-legend">Job Responsibilities</legend>
+                    <textarea className="textarea"
+                        name='responsibilities' placeholder="Job Responsibilities (separate by comma)"></textarea>
 
                 </fieldset>
+
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                    <legend className="fieldset-legend">HR Related Info</legend>
+
+                    <label className="label">HR Name</label>
+                    <input type="text" name='hr_name' className="input" placeholder="HR Name" />
+
+                    <label className="label">HR Email</label>
+                    <input type="email"
+                        name='company'
+                        className="input" placeholder="HR Email" />
+
+                    <input type="submit" value="Add Job" className='btn' />
+                </fieldset>
+
                 {/*  */}
                 <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
                     <legend className="fieldset-legend">Basic Info</legend>
