e3c71ea621c7f9190ce81350a05de58f695a5c84
Samiul2024 <mdsamiullahossen@gmail.com>
2026-02-14 12:16:38 +0600

    applications shown to the table , added options...

--- src/pages/ViewApplications/ViewApplications.jsx
+++ src/pages/ViewApplications/ViewApplications.jsx
@@ -6,9 +6,46 @@ const ViewApplications = () => {
     const { job_id } = useParams();
     const applications = useLoaderData();
 
+    const handleStatusChange = (e, application) => {
+        console.log(e.target.value, application);
+    }
+
     return (
         <div>
             <h2 className="text-4xl">{applications.length}Applications for: {job_id}</h2>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th> Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            applications.map(application => <tr key={application._id}>
+                                <th>1</th>
+                                <td>{application.applicant}</td>
+                                <td>Quality Control Specialist</td>
+                                <td>
+                                    <select onChange={handleStatusChange} defaultValue={application.status} className="select">
+                                        <option disabled={true}>Update Status</option>
+                                        <option>Pending</option>
+                                        <option>Call For Interview</option>
+                                        <option>Hired</option>
+                                        <option>Rejected</option>
+                                    </select>
+                                </td>
+                            </tr>
+                            )
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
