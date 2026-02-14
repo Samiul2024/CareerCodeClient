b480b331dbdd7d493d705544eac4e4a331670011
Samiul2024 <mdsamiullahossen@gmail.com>
2026-02-14 23:51:59 +0600

    applicants status upgradation ready...

--- src/pages/ViewApplications/ViewApplications.jsx
+++ src/pages/ViewApplications/ViewApplications.jsx
@@ -1,13 +1,31 @@
+import axios from 'axios';
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
+import Swal from 'sweetalert2';
 
 const ViewApplications = () => {
 
     const { job_id } = useParams();
     const applications = useLoaderData();
 
-    const handleStatusChange = (e, application) => {
-        console.log(e.target.value, application);
+    const handleStatusChange = (e, app_id) => {
+        console.log(e.target.value, app_id);
+
+        axios.patch(`http://localhost:3000/applications/${app_id}`, { status: e.target.value })
+            .then(res => {
+                console.log(res.data);
+                if (res.data.modifiedCount) {
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Application status updated successfully",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
+            .catch(error => console.log(error))
+
     }
 
     return (
@@ -32,7 +50,7 @@ const ViewApplications = () => {
                                 <td>{application.applicant}</td>
                                 <td>Quality Control Specialist</td>
                                 <td>
-                                    <select onChange={handleStatusChange} defaultValue={application.status} className="select">
+                                    <select onChange={e => handleStatusChange(e, application._id)} defaultValue={application.status} className="select">
                                         <option disabled={true}>Update Status</option>
                                         <option>Pending</option>
                                         <option>Call For Interview</option>
